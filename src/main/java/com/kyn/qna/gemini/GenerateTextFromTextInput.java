61764d67ec9a1d05f8efb1d4d0906adf61c4c069
kyn-317 <kynauto0317@gmail.com>
2025-05-27 21:33:15 +0900

    genAITutorial

--- src/main/java/com/kyn/qna/gemini/GenerateTextFromTextInput.java
+++ src/main/java/com/kyn/qna/gemini/GenerateTextFromTextInput.java
@@ -1,19 +1,58 @@
 package com.kyn.qna.gemini;
 
-import com.google.genai.Client;
-import com.google.genai.types.GenerateContentResponse;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.google.common.collect.ImmutableList;
+import com.google.genai.Client;
+import com.google.genai.ResponseStream;
+import com.google.genai.types.*;
+import com.google.gson.Gson;
+
+@Service
 public class GenerateTextFromTextInput {
-    public static void main(String[] args) {
+
+    @Value("${GOOGLE_API_KEY}")
+    private String googleApiKey;
+    
+    public void generateTextFromTextInput(String inputText) {
         // The client gets the API key from the environment variable `GOOGLE_API_KEY`.
-        Client client = new Client();
+        Client client = Client.builder().apiKey(googleApiKey).build();
+        Gson gson = new Gson();
     
-        GenerateContentResponse response =
-            client.models.generateContent(
-                "gemini-2.0-flash",
-                "Explain how AI works in a few words",
-                null);
     
-        System.out.println(response.text());
+        String model = "gemma-3n-e4b-it";
+        List<Content> contents = ImmutableList.of(
+          Content.builder()
+            .role("user")
+            .parts(ImmutableList.of(
+              Part.fromText(inputText)
+            ))
+            .build()
+        );
+        GenerateContentConfig config =
+          GenerateContentConfig
+          .builder()
+          .responseMimeType("text/plain")
+          .build();
+    
+        ResponseStream<GenerateContentResponse> responseStream = client.models.generateContentStream(model, contents, config);
+    
+        for (GenerateContentResponse res : responseStream) {
+          if (res.candidates().isEmpty() || res.candidates().get().get(0).content().isEmpty() || res.candidates().get().get(0).content().get().parts().isEmpty()) {
+            continue;
+          }
+    
+          List<Part> parts = res.candidates().get().get(0).content().get().parts().get();
+          for (Part part : parts) {
+            System.out.println(part.text());
+          }
+        }
+    
+        responseStream.close();
       }
 }
+
+
