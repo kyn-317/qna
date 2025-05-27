e12791baa6797562c8c25605537d1e89a75d7a09
kyn-317 <kynauto0317@gmail.com>
2025-05-27 21:02:09 +0900

    first commit

---
+++ src/main/java/com/kyn/qna/gemini/GenerateTextFromTextInput.java
@@ -0,0 +1,19 @@
+package com.kyn.qna.gemini;
+
+import com.google.genai.Client;
+import com.google.genai.types.GenerateContentResponse;
+
+public class GenerateTextFromTextInput {
+    public static void main(String[] args) {
+        // The client gets the API key from the environment variable `GOOGLE_API_KEY`.
+        Client client = new Client();
+    
+        GenerateContentResponse response =
+            client.models.generateContent(
+                "gemini-2.0-flash",
+                "Explain how AI works in a few words",
+                null);
+    
+        System.out.println(response.text());
+      }
+}
