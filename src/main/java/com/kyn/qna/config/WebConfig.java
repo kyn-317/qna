448c022121a9dc06dec994d15563a53c0f9f81d5
kyn-317 <kynauto0317@gmail.com>
2025-06-05 19:59:53 +0900

    frontend set

--- src/main/java/com/kyn/qna/config/WebConfig.java
+++ src/main/java/com/kyn/qna/config/WebConfig.java
@@ -1,13 +1,29 @@
 package com.kyn.qna.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.MediaType;
 import org.springframework.web.reactive.config.CorsRegistry;
 import org.springframework.web.reactive.config.EnableWebFlux;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.*;
+import static org.springframework.web.reactive.function.server.RouterFunctions.*;
 
 @Configuration
 @EnableWebFlux
 public class WebConfig implements WebFluxConfigurer {
+
+    @Bean
+    public RouterFunction<ServerResponse> staticResourceRouter() {
+        return route(GET("/"), 
+            request -> ServerResponse.ok()
+                .contentType(MediaType.TEXT_HTML)
+                .bodyValue(new ClassPathResource("static/index.html")));
+    }
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
